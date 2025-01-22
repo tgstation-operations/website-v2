7f29db8effd86bc95a131ca477c0280dd12cd51a
lorwp <lorwp@lorwp.cc>
2025-01-22 20:42:42 +1100

    WIP

---
+++ default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  config,
+  dream2nix,
+  ...
+}: {
+  imports = [
+    dream2nix.modules.dream2nix.nodejs-package-lock-v3
+    dream2nix.modules.dream2nix.nodejs-granular-v3
+  ];
+
+  mkDerivation = {
+    src = ./.
+  };
+
+  deps = {nixpkgs, ...}: {
+    inherit
+      (nixpkgs)
+      fetchFromGitHub
+      stdenv
+      ;
+  };
+
+  nodejs-package-lock-v3 = {
+    packageLockFile = "${config.mkDerivation.src}/package-lock.json";
+  };
+
+  name = "website-v2";
+  version = "1.0.0";
+}
