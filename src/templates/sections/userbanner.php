9186fac3425391ee2ef6ff9351592d2814961432
striders13 <anonimustranger@gmail.com>
2025-03-07 09:52:06 +0500

    yeah that doesn't work rn

--- src/templates/sections/userbanner.php
+++ src/templates/sections/userbanner.php
@@ -3,8 +3,8 @@
 	<div class="mt-4 text-center"><small class="d-block mb-1"> A word from one of our players</small>
 		<!-- TODO: make 50buck.php and getlogo.php available in this repo (And merge them? What do they do?) and use relative links here-->
 		<img class="w-100" loading="lazy" src="./dynamicimages/pickrandombanner.php" />
-		<a class="small d-block mt-1" href="https://tgstation13.org/phpBB/viewtopic.php?f=2&t=3371">
+		<!-- <a class="small d-block mt-1" href="https://tgstation13.org/phpBB/viewtopic.php?f=2&t=3371">
 			How to get your banner up here
-		</a>
+		</a> -->
 	</div>
 </section>
