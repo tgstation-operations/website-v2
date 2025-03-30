1cc25123d6fc70927604c886dd41f23e099a0864
Nick <nick@nfreader.net>
2025-03-30 09:44:49 -0400

    Handling data

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,14 +2,35 @@ const fs = require("fs");
 const path = require("path");
 const shortcodes = require("./utils/shortcodes");
 module.exports = async function (eleventyConfig) {
-  const alertsData = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "alerts.json"), "utf-8")
-  );
-  const serverData = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "servers.json"), "utf-8")
-  );
+  function readJsonFile(filename, fallback) {
+    const primaryPath = path.join(__dirname, filename);
+    const fallbackPath = path.join(__dirname, fallback);
+    const filePath = fs.existsSync(primaryPath) ? primaryPath : fallbackPath;
+    return JSON.parse(fs.readFileSync(filePath, "utf-8"));
+  }
+
+  const alertsData = readJsonFile("alerts.json", "example-alerts.json");
+  const serverData = readJsonFile("servers.json", "example-servers.json");
+  const navData = readJsonFile("nav.json", "example-nav.json");
+
+  alertsData.forEach((alert) => {
+    let validAlerts = ["danger", "warning", "info"];
+    if (alert && !validAlerts.includes(alert.type)) {
+      console.log(
+        `CRITICAL ERROR: INVALID ALERT TYPE SPECIFIED: ${alert.type}`
+      );
+      return;
+    }
+    if (alert.type === "info") {
+      alert.type = "accent";
+    }
+    if (alert.type === "warning") {
+      alert.type = "severe";
+    }
+  });
   eleventyConfig.addGlobalData("alerts", alertsData);
   eleventyConfig.addGlobalData("servers", serverData);
+  eleventyConfig.addGlobalData("nav", navData);
   eleventyConfig.addNunjucksAsyncShortcode("webpack", shortcodes.webpack);
   return {
     dir: {
