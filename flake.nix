4cc962a0429f4a1685b6aea108cd28d67213d125
Nick <nick@nfreader.net>
2025-04-05 12:31:52 -0400

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "tgstation website";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = nixpkgs.legacyPackages.${system};
+      });
+    in
+    {
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.buildNpmPackage {
+          name = "tgstation-website";
+          buildInputs = with pkgs; [
+            nodejs_22
+          ];
+          src = ./.;
+          npmDepsHash = "sha256-+YrFWZEQvBfrlUOB0EwlN3sNBgqnc78HD7KNPJP086Q=";
+          npmBuild = "npm run build";
+          installPhase = ''
+            mkdir $out
+            cp -r _site/* $out
+          '';
+        };
+      });
+    };
+}
